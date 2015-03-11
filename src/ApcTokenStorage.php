2b754d3e2fbd24a43656336461b6ab511809ced9
fedemotta <fedemotta@gmail.com>
2015-03-11 17:16:26 -0300

    Adding ApcTokenStorage class and test

---
+++ src/ApcTokenStorage.php
@@ -0,0 +1,47 @@
+<?php
+namespace F3\AppNexusClient;
+
+/**
+ * ApcTokenStorage
+ *
+ * @uses TokenStorage
+ * @package
+ * @version $id$
+ * @copyright Federico Nicolás Motta
+ * @author Federico Nicolás Motta <fedemotta@gmail.com>
+ * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
+ */
+class ApcTokenStorage implements TokenStorage
+{
+    private $prefix;
+    private $ttl;
+
+    public function __construct($prefix = '',  $ttl = 0 )
+    {
+        $this->prefix = $prefix;
+        $this->ttl = $ttl;
+    }
+
+    /**
+     * set token
+     *
+     * @param string $username
+     * @param string $token
+     * @return bool
+     */
+    public function set($username, $token)
+    {
+        return apc_store($this->prefix.$username, $token, $this->ttl);
+    }
+
+    /**
+     * get token
+     *
+     * @param string $username
+     * @return string|false
+     */
+    public function get($username)
+    {
+        return apc_fetch($this->prefix.$username);
+    }
+}
